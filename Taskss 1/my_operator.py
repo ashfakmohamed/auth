4f12744399993f4dbb0fdcd395e4bca75683f98f
Mohamed Ashfak <74375272+ashfakmohamed@users.noreply.github.com>
2024-06-07 18:14:46 +0530

    Add files via upload

---
+++ Taskss 1/my_operator.py
@@ -0,0 +1,12 @@
+# Problem 1
+
+import re
+
+# Here The Given text
+text = '{"orders":[{"id":1},{"id":2},{"id":3},{"id":4},{"id":5},{"id":6},{"id":7},{"id":8},{"id":9},{"id":10},{"id":11},{"id":648},{"id":649},{"id":650},{"id":651},{"id":652},{"id":653}],"errors":[{"code":3,"message":"[PHP Warning #2] count(): Parameter must be an array or an object that implements Countable (153)"}]}'
+
+# Regex to find all numbers in the orders list
+numbers = re.findall(r'{"id":(\d+)}', text)
+
+# For Printing the extracted numbers
+print(numbers)
